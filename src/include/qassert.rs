e7fdbcf52e7d8379204b88e66fc2b6ab6980090e
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2025-12-20 20:00:20 -0800

    Add folders

---
+++ src/include/qassert.rs
@@ -0,0 +1,10 @@
+static Q_DEFINE_THIS_FILE: &'static [u8] = __FILE__;
+static Q_DEFINE_THIS_FILE: &'static Fn = |name: &str| {
+    return
+};
+
+type Q_NORETURN = ();
+
+fn Q_onAssert(module: &str, location: isize) -> Q_NORETURN {
+
+}
